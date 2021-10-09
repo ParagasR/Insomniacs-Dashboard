2179641cd8cc3ad5d5c8b489ff0a860323d9e502
@bagapillai <pbperumal@gmail.com>
2021-10-08 18:31:41 -0600

    html skeleton first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
-var OMDBApiURL = "http://www.omdbapi.com/?i=tt3896198&apikey=e3bfc809&s=diana"
-var TMDBApiURL = "https://api.themoviedb.org/3/movie/550?api_key=c7fa5c32a18aa2a0e3ea8e061504176d"
+var OMDBApiURL = "http://www.omdbapi.com/?i=tt3896198&apikey=e3bfc809&s=diana";
+var TMDBApiURL =
+  "https://api.themoviedb.org/3/movie/550?api_key=c7fa5c32a18aa2a0e3ea8e061504176d";
 
 fetch(TMDBApiURL)
-.then (function (response){
-    return response.json()
-})
-.then (function (data){
-    console.log(data)
-})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
